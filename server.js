c138d8f525b699bf56a262fba271705a7a4d50ba
magvas <info@magvas>
2016-03-12 22:50:34 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var dbaccess = require('./lib/dbaccess');
+
+var server = http.createServer(function(req,res){
+   res.writeHead(200,{'content-type':'text/json'});
+   
+   if (req.method == 'POST') {
+        
+        req.on('data', function (data) {
+            var body = '';
+            body += data;
+            dbaccess.getAllWinesOfVintner(body,res,addResultToOutput);
+        });
+   }
+   else{
+        dbaccess.getAllWinesOfVintner('Schneider',res,addResultToOutput);
+        console.log('Response build!');   
+   }
+});
+
+var addResultToOutput = function (result,response){
+    response.write(JSON.stringify(result));
+    response.end();
+}
+
+server.listen(10000);
